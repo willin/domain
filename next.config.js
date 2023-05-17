ce89efaa8d7aa682e4c6e0bed9655f7517831056
Willin Wang <willin@willin.org>
2023-05-17 22:30:07 +0800

    feat: init

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  runtime: 'edge',
-}
+  runtime: 'edge'
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
