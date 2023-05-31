336e5fcb0a55723a260eedc43b0af4368227d841
Willin Wang <willin@willin.org>
2023-05-31 17:57:43 +0800

    fix: z-index

--- app/[lang]/header.tsx
+++ app/[lang]/header.tsx
@@ -5,7 +5,7 @@ import { Locale } from '@/i18n-config';
 
 export function MainHeader({ lang }: { lang: Locale }) {
   return (
-    <header className='sticky top-0 flex justify-center w-full z-20 opacity-90 hover:opacity-100 bg-base-100 mb-4'>
+    <header className='sticky top-0 flex justify-center w-full z-[9999] opacity-90 hover:opacity-100 bg-base-100 mb-4'>
       <div className='navbar bg-base-100'>
         <div className='navbar-start'>
           <Link href={`/${lang}`} className='btn btn-ghost normal-case text-xl'>
