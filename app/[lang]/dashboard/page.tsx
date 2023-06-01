dc1cdc18094b5930a3dc3c107d6ac7cd9279b9d8
Willin Wang <willin@willin.org>
2023-06-01 16:18:02 +0800

    feat: limitation

--- app/[lang]/dashboard/page.tsx
+++ app/[lang]/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { Locale } from '@/i18n-config';
 import { translation } from '@/lib/i18n';
 import { CreateAndTitle } from './create';
 import { DomainList } from './table';
+import { MAX_LIMIT_FOLLOWER, MAX_LIMIT_USER, MAX_LIMIT_VIP } from '@/lib/config';
 
 export const revalidate = 60;
 
@@ -27,6 +28,29 @@ export default async function Page({ params: { lang } }: { params: { lang: Local
               <DomainList lang={lang} />
             </tbody>
           </table>
+        </div>
+        <div>
+          <h3 className='pt-4 text-info'>{t('common.limit_info')}</h3>
+          <ul className='list-disc pl-4'>
+            <li>
+              {t('common.user')}: <span className='badge badge-primary'>{MAX_LIMIT_USER}</span>
+            </li>
+            <li>
+              {t('common.follower')}: <span className='badge badge-secondary'>{MAX_LIMIT_FOLLOWER}</span>
+              <a href='https://github.com/willin' target='_blank' className='btn btn-xs btn-secondary ml-2'>
+                {t('common.follow')}
+              </a>
+            </li>
+            <li>
+              {t('common.vip')}: <span className='badge badge-accent'>{MAX_LIMIT_VIP}</span>
+              <a
+                target='_blank'
+                className='btn btn-xs btn-accent ml-2'
+                href={lang === 'zh' ? 'https://afdian.net/a/willin' : 'https://github.com/sponsors/willin'}>
+                {t('common.donate')}
+              </a>
+            </li>
+          </ul>
         </div>
       </section>
     </main>
